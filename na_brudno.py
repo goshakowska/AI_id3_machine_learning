b5b1d6e6c8b2f73d408f82c43da264ddb7dd9c50
mkozlo12 <01169195@pw.edu.pl>
2022-12-19 13:03:18 +0100

    Działające tworzenie drzewa!!!!

--- na_brudno.py
+++ na_brudno.py
@@ -108,10 +108,22 @@ def average_information(data_attribute_subset: pd.DataFrame, target_attribute_va
     for each_value in values_to_analyse:
         value_fraction = data_attribute_subset.value_counts()[each_value].sum()/total_class_number
         selected_rows = data_attribute_subset.where(data_attribute_subset == each_value)
-        rows_to_analyse = target_attribute_values.loc[selected_rows.dropna().index.values.tolist()]  # CZY TU JEST BŁĄD? z .copy()??
+        rows_to_analyse = target_attribute_values.copy().loc[selected_rows.dropna().index.values.tolist()]  # CZY TU JEST BŁĄD? z .copy()??
         attribute_average_information += value_fraction*entropy(rows_to_analyse)
     return data_attribute_subset.name, attribute_average_information
 
+
+def find_best_depth(train_dataset: pd.DataFrame, train_target: pd.Series, validation_dataset: pd.DataFrame, validation_target: pd.Series, stop_max_depth: int):
+    value_differences_counter = {}
+    for each_depth in range(2, stop_max_depth):
+        new_tree = ID3Tree(each_depth)
+        new_tree.set_root(new_tree.build_ID3_tree(train_dataset, train_target, each_depth))
+        train_target = new_tree.predict_target_attribute_value(train_dataset)
+        predicted_validation_target = new_tree.predict_target_attribute_value(validation_dataset)
+        number_of_different_values_in_rows = validation_target.compare(predicted_validation_target).count()[0]
+        value_differences_counter[each_depth] = number_of_different_values_in_rows
+    best_max_depth = max(value_differences_counter, key=value_differences_counter.get)
+    return best_max_depth
 
 class ID3Node:
     def __init__(self, attribute_name: str, values_of_attribute, children_nodes: ['ID3Node'] = None):
@@ -121,6 +133,12 @@ class ID3Node:
         self._children_nodes = children_nodes
 
     def predict_next_node(self, data_row: pd.Series):
+        """
+        Method that for each provided row iterates through tree branches dictionary in order to find the value of
+        the target attributee
+        :param data_row:
+        :return:
+        """
         print(data_row)
         print(self._attribute_name)
         print()
@@ -131,14 +149,27 @@ class ID3Node:
         #     return value
         try:
             next_node = self._tree_branches_dictionary[value]
+
             if not isinstance(next_node, ID3Node):
                 print(next_node)
                 return next_node
         except KeyError:
+            most_frequent_target_attribute_value = []
+            for child_node in self._tree_branches_dictionary.values():
+                if not isinstance(child_node, ID3Node):
+                    most_frequent_target_attribute_value.append(child_node)
+                    continue
+                most_frequent_target_attribute_value.append(child_node.predict_next_node(data_row))
+            print(most_frequent_target_attribute_value)
 
-            if not isinstance(next_node, ID3Node):
-                return next_node
-
+            most_frequent_value = max(set(most_frequent_target_attribute_value), key=most_frequent_target_attribute_value.count)
+            return most_frequent_value
+            # print("UWAGA!")
+            # print("nie ma gałęzi")
+            # print(self._tree_branches_dictionary)
+            # return self._tree_branches_dictionary
+            # if not isinstance(next_node, ID3Node):
+            #     return next_node
         return next_node.predict_next_node(data_row)
 
 
@@ -198,10 +229,10 @@ class ID3Tree:
 
     def predict_target_attribute_value(self, train_dataset: pd.DataFrame):
         root_node = self.get_root()
-        for _, each_row in train_dataset.iterrows():
-            root_node.predict_next_node(each_row)
-        return train_dataset
-        # return train_dataset.apply(lambda data_row: root_node.predict_next_node(data_row), axis=1)
+        # for _, each_row in train_dataset.iterrows():
+        #     root_node.predict_next_node(each_row)
+        # return train_dataset
+        return train_dataset.apply(lambda data_row: root_node.predict_next_node(data_row), axis=1)
 
 
 
@@ -216,22 +247,14 @@ data, target = divide_dataset_for_target_attribute(formatted_data, 'irradiat')
 formatted_data_2 = formatted_data.where(formatted_data == 'irradiat')
 
 
-x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.25)
-tree = ID3Tree(5)
-total_class_number = target.value_counts()
-print(total_class_number[0])
-tree.set_root(tree.build_ID3_tree(x_train, y_train, tree.get_max_depth()))
-y_train_prediction = tree.predict_target_attribute_value(x_train)
-y_test_prediction = tree.predict_target_attribute_value(x_test)
-print(y_test_prediction)
-print(y_train_prediction)
-# attribute_information_gains = {}
-# for each_column in data.columns:
-#     # print(data[each_column].name)
-#     print(average_information(data[each_column], target))
-#     # attr_name, av_inf = average_information(data[each_column], target)
-#     attr_name, information = information_gain(entropy(target), average_information(data[each_column], target)[0], average_information(data[each_column], target)[1])
-#     attribute_information_gains[attr_name] = information
-# print(find_best_attribute(attribute_information_gains))
+x_train, x_rem, y_train, y_rem = train_test_split(data, target, train_size=0.8)
+
+x_valid, x_test, y_valid, y_test = train_test_split(x_rem, y_rem, test_size=0.5)
+
+
+pass
+depth = find_best_depth(x_train, y_train, x_valid, y_valid, 6)
+print(depth)
+
 pass
 
